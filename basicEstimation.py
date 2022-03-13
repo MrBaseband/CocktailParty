77279fe8a4030ae92cb15d7c4d7211a407b1014e
unknown <balram.pillai@gmail.com>
2022-03-13 13:31:32 +0530

    Structured how basicEstimation.py would work

--- basicEstimation.py
+++ basicEstimation.py
@@ -1,9 +1,25 @@
+from functools import partial
 from scipy.io.wavfile import read, write
 # parsing .wav file
 
-# def seperate(m1,m2)
+# def estimateWMatrix(m1,m2)
 # Math here
-# return a,b,confidenceNumber # estimates
+# return W,confidenceNumber # estimates
 
-# Basic wav parsing to be done here
 
+# input parse .wavs -> m1,m2
+# Call: estimate W
+# Build signals [a,b] = W.[m1 m2]
+
+# Write outputs a,b into .wavs
+
+
+# TODO
+# 1. Basic math part
+# 2. Wav parsing
+# 3. Three test cases 
+    # a. (300 Hz,400 Hz) = (30,70) 
+    # b. (300 Hz,400 Hz) = (70,30)
+    # c. (300 Hz,400 Hz) = (100,0)  
+    # d. (300 Hz,400 Hz) = (0,100)  
+    # These cover relative amplitudes, what about phase delays?
