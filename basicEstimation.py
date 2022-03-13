084096dfff0cd400e9722edeac5a4ee9d7fc2df3
unknown <balram.pillai@gmail.com>
2022-03-13 13:09:21 +0530

    WIP Included basicEstimation.py

---
+++ basicEstimation.py
@@ -0,0 +1,9 @@
+from scipy.io.wavfile import read, write
+# parsing .wav file
+
+# def seperate(m1,m2)
+# Math here
+# return a,b,confidenceNumber # estimates
+
+# Basic wav parsing to be done here
+
